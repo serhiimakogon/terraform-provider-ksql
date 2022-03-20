ad390cf4d394f30a65b9c51f542d11a26217f7ad
Gabriel Aranha Martins <gabriel.aranha.martins@gmail.com>
2022-03-20 15:20:25 -0300

    Improve docs

--- examples/main.tf
+++ examples/main.tf
@@ -2,11 +2,24 @@ terraform {
   required_providers {
     ksql = {
       source = "gabriel-aranha/ksql"
-      version = "1.0.4-pre"
+      version = "1.0.5-pre"
     }
   }
 }
 
 provider "ksql" {
   # Configuration options
+}
+
+data "ksql_stream" "main" {
+  name = "TEST_STREAM"
+}
+
+data "ksql_streams" "main" {
+  tag = "TEST"
+}
+
+resource "ksql_stream" "main" {
+  name = "ANOTHER_STREAM"
+  query = "AS SELECT * FROM TEST_STREAM;"
 }
